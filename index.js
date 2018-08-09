a5d49a38da57b439b8a88652725fe9cc572665f1
Lauren West <laurenwest12@gmail.com>
2018-08-09 23:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ function scuberGreetingForFeet (ride) {
   if (ride <= 400) {
     result = 'This one is on me!'
   } 
-  
     else {
     result = 'This one is on you!';
   }
-  
   return result
 }
 
