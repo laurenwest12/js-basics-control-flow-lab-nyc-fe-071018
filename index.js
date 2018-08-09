f4a49509d5535c084b806319f25a8d8d55ec0bf7
Lauren West <laurenwest12@gmail.com>
2018-08-09 23:47:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function scuberGreetingForFeet (ride) {
 }
 
 function ternaryCheckCity (city){
-  city = 'New York City' ? 'Ok, sounds good.' : 'No go.';
+  return ? 'Ok, sounds good.' : 'No go.';
 }
