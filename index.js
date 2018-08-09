28977dabae5728b63ea2743c1f537fd4b71a6e7c
Lauren West <laurenwest12@gmail.com>
2018-08-09 23:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ function ternaryCheckCity (city){
 function switchOnCharmFromTip (tip) {
   if (tip === 'generous'){
     return 'Thank you so much.'
-  } else if (tip === 'not so generous') {
-    return ''
+  } else if (tip === 'not as generous') {
+    return 'Thank you.'
+  } else {
+    return 
   }
 }
